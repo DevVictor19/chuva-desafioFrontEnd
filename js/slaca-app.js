3774d4c57ee5963828acea74478f6d88f567f8bd
Antonio Victor <antoniovictor12@live.com>
2022-05-09 17:20:59 -0300

    resumo pronto

--- js/slaca-app.js
+++ js/slaca-app.js
@@ -31,3 +31,35 @@ const slaca_sidemenu = () => {
   }
 };
 slaca_sidemenu();
+
+const slaca_showMoreText = () => {
+  // selecao elementos
+  const resumeShowMoreBtn = document.getElementById("resume-text-showMore");
+
+  // funcoes funcionamento
+  const hideElementById = (id) => {
+    const element = document.getElementById(id);
+    element.style.display = "none";
+  };
+
+  const showElementByClass = (className) => {
+    const element = document.getElementsByClassName(className);
+    element[0].style.display = "block";
+  };
+
+  const showTextsHandler = (ellipsis_id, button_id, clippedText_class) => {
+    hideElementById(ellipsis_id);
+    hideElementById(button_id);
+    showElementByClass(clippedText_class);
+  };
+
+  // eventos
+  resumeShowMoreBtn.addEventListener("click", () => {
+    showTextsHandler(
+      "resume-ellipsis",
+      "resume-text-showMore",
+      "resume-text-clipped"
+    );
+  });
+};
+slaca_showMoreText();

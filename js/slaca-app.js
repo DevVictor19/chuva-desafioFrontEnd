12b82fed711350c2047087ad98b8b017ded2abdb
Antonio Victor <antoniovictor12@live.com>
2022-05-10 15:40:44 -0300

    gerenciar topicos pronto

--- js/slaca-app.js
+++ js/slaca-app.js
@@ -1,42 +1,5 @@
-const slaca_sidemenu = () => {
-  // selecao dos elementos
-  const navigationAnchors = document.querySelectorAll(
-    ".sidemenu-navigation ul li a"
-  );
-
-  const initialActiveLi = document.getElementsByClassName("sidemenu-active");
-  const activeSpan = document.querySelector(".sidemenu-active span");
-
-  // funcoes funcionamento
-  let lastActiveLi = initialActiveLi[0];
-
-  const selectItemMenu = (event) => {
-    const currentClickedLi = event.path[1];
-    if (lastActiveLi === currentClickedLi) {
-      return;
-    }
-
-    lastActiveLi.classList.remove("sidemenu-active");
-    lastActiveLi.removeChild(activeSpan);
-
-    currentClickedLi.classList.add("sidemenu-active");
-    currentClickedLi.appendChild(activeSpan);
-
-    lastActiveLi = currentClickedLi;
-  };
-
-  // eventos
-  for (let anchor of navigationAnchors) {
-    anchor.addEventListener("click", selectItemMenu);
-  }
-};
-slaca_sidemenu();
-
-const slaca_showMoreText = () => {
-  // selecao elementos
-  const resumeShowMoreBtn = document.getElementById("resume-text-showMore");
-
-  // funcoes funcionamento
+const slaca_app = () => {
+  // funcoes globais
   const setDisplayNoneById = (id) => {
     const element = document.getElementById(id);
     element.style.display = "none";
@@ -47,13 +10,20 @@ const slaca_showMoreText = () => {
     element.style.display = "block";
   };
 
+  const setDisplayFlexById = (id) => {
+    const element = document.getElementById(id);
+    element.style.display = "flex";
+  };
+
+  // expandir a secao resumo -> inicio
+  const resumeShowMoreBtn = document.getElementById("resume-text-showMore");
+
   const showTextsHandler = (ellipsis_id, button_id, clippedText_id) => {
     setDisplayNoneById(ellipsis_id);
     setDisplayNoneById(button_id);
     setDisplayBlockById(clippedText_id);
   };
 
-  // eventos
   resumeShowMoreBtn.addEventListener("click", () => {
     showTextsHandler(
       "resume-ellipsis",
@@ -61,5 +31,63 @@ const slaca_showMoreText = () => {
       "resume-text-clipped"
     );
   });
+  // expandir a secao resumo -> fim
+
+  // adicionar topicos -> inicio
+  const createTopicBtn = document.getElementById("create-topic-btn");
+  const sendNewTopicBtn = document.getElementById(
+    "discussions-form-actions-submit"
+  );
+  const remakeTopicBtn = document.getElementById("remake-topic-btn");
+
+  const createTopicBtnHandler = () => {
+    setDisplayNoneById("discussions-addTopic");
+    setDisplayFlexById("discussions-writeNewTopic");
+  };
+
+  const sendNewTopicBtnHandler = () => {
+    const assuntoInputText = document.getElementById("assunto");
+    const conteudoTextArea = document.getElementById("conteudo");
+    let inputsAreValid = true;
+
+    // verificar se algum dos inputs se encontra vazio
+    if (assuntoInputText.value.trim() === "") {
+      assuntoInputText.style.borderColor = "red";
+      inputsAreValid = false;
+    }
+
+    if (conteudoTextArea.value.trim() === "") {
+      conteudoTextArea.style.borderColor = "red";
+      conteudoTextArea.style.borderBottom = "2px solid red";
+      inputsAreValid = false;
+    }
+
+    if (!inputsAreValid) {
+      return;
+    }
+
+    // resetando estilos
+    assuntoInputText.style.borderColor = "#ccc";
+    conteudoTextArea.style.borderColor = "#ccc";
+    conteudoTextArea.style.borderBottom = "none";
+
+    // renderizando a outra tela
+    setDisplayNoneById("discussions-writeNewTopic");
+    setDisplayFlexById("discussions-topicSent");
+
+    // limpando inputs
+    assuntoInputText.value = "";
+    conteudoTextArea.value = "";
+  };
+
+  const remakeTopicBtnHandler = () => {
+    setDisplayNoneById("discussions-topicSent");
+    setDisplayFlexById("discussions-addTopic");
+  };
+
+  createTopicBtn.addEventListener("click", createTopicBtnHandler);
+  sendNewTopicBtn.addEventListener("click", sendNewTopicBtnHandler);
+  remakeTopicBtn.addEventListener("click", remakeTopicBtnHandler);
+  // adicionar topicos -> fim
 };
-slaca_showMoreText();
+slaca_app();
